f0504a7f2b220eeb9867f28c3836e741a97ced94
jim-desktop <jianyu234@gmail.com>
2024-06-05 02:04:47 -0400

    commit

--- main_breakout_PPO_cleanrl.py
+++ main_breakout_PPO_cleanrl.py
@@ -34,61 +34,60 @@ details of openai/baselines’ PPO."
 
 @dataclass
 class Args:
-    def __init__(self):
-        self.exp_name: str = os.path.basename(__file__)[: -len(".py")]
-        self.seed: int = 1
-        self.torch_deterministic: bool = True
-        """if toggled, `torch.backends.cudnn.deterministic=False`"""
-        self.cuda: bool = True
-        """if toggled, cuda will be enabled by default"""
-        self.track: bool = False
-        """if toggled, this experiment will be tracked with Weights and Biases"""
-        self.wandb_project_name: str = "PPO_cleanRL"
-        """the wandb's project name"""
-        self.wandb_entity: str = None
-        """the entity (team) of wandb's project"""
-        self.capture_video: bool = False
-        """whether to capture videos of the agent performances (check out `videos` folder)"""
+    exp_name: str = os.path.basename(__file__)[: -len(".py")]
+    seed: int = 1
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = True
+    """if toggled, cuda will be enabled by default"""
+    track: bool = False
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_project_name: str = "PPO_cleanRL"
+    """the wandb's project name"""
+    wandb_entity: str = None
+    """the entity (team) of wandb's project"""
+    capture_video: bool = False
+    """whether to capture videos of the agent performances (check out `videos` folder)"""
 
-        # Algorithm specific arguments
-        self.env_id: str = "BreakoutNoFrameskip-v4"
-        self.total_timesteps: int = 10000000
-        self.learning_rate: float = 2.5e-4
-        self.num_envs: int = 8  # 8
-        self.num_steps: int = 128  # 128
-        """the number of steps to run in each environment per policy rollout"""
-        self.anneal_lr: bool = True
-        """Toggle learning rate annealing for policy and value networks"""
-        self.gamma: float = 0.99
-        """the discount factor gamma"""
-        self.gae_lambda: float = 0.95
-        """the lambda for the general advantage estimation"""
-        self.num_minibatches: int = 4  # 4
-        """the number of mini-batches"""
-        self.update_epochs: int = 4  # 4
-        """the K epochs to update the policy"""
-        self.norm_adv: bool = True
-        """Toggles advantages normalization"""
-        self.clip_coef: float = 0.1
-        """the surrogate clipping coefficient"""
-        self.clip_vloss: bool = True
-        """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
-        self.ent_coef: float = 0.01
-        """coefficient of the entropy"""
-        self.vf_coef: float = 0.5
-        """coefficient of the value function"""
-        self.max_grad_norm: float = 0.5
-        """the maximum norm for the gradient clipping"""
-        self.target_kl: float = None
-        """the target KL divergence threshold"""
+    # Algorithm specific arguments
+    env_id: str = "BreakoutNoFrameskip-v4"
+    total_timesteps: int = 10000000
+    learning_rate: float = 2.5e-4
+    num_envs: int = 8  # 8
+    num_steps: int = 128  # 128
+    """the number of steps to run in each environment per policy rollout"""
+    anneal_lr: bool = True
+    """Toggle learning rate annealing for policy and value networks"""
+    gamma: float = 0.99
+    """the discount factor gamma"""
+    gae_lambda: float = 0.95
+    """the lambda for the general advantage estimation"""
+    num_minibatches: int = 4  # 4
+    """the number of mini-batches"""
+    update_epochs: int = 4  # 4
+    """the K epochs to update the policy"""
+    norm_adv: bool = True
+    """Toggles advantages normalization"""
+    clip_coef: float = 0.1
+    """the surrogate clipping coefficient"""
+    clip_vloss: bool = True
+    """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
+    ent_coef: float = 0.01
+    """coefficient of the entropy"""
+    vf_coef: float = 0.5
+    """coefficient of the value function"""
+    max_grad_norm: float = 0.5
+    """the maximum norm for the gradient clipping"""
+    target_kl: float = None
+    """the target KL divergence threshold"""
 
-        # to be filled in runtime
-        self.batch_size: int = 0
-        """the batch size (computed in runtime)"""
-        self.minibatch_size: int = 0
-        """the mini-batch size (computed in runtime)"""
-        self.num_iterations: int = 0
-        """the number of iterations (computed in runtime)"""
+    # to be filled in runtime
+    batch_size: int = 0
+    """the batch size (computed in runtime)"""
+    minibatch_size: int = 0
+    """the mini-batch size (computed in runtime)"""
+    num_iterations: int = 0
+    """the number of iterations (computed in runtime)"""
 
 
 def make_env(env_id, idx, capture_video, run_name):
